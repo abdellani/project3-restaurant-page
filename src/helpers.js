021569c411e79df4587fa88296252f2616cf552d
abdellani <abdellani@gmail.com>
2019-09-18 16:14:52 +0100

    Add contact module

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-function createElement(type, value = null) {
+const createElement= (type, value = null) => {
   const node = document.createElement(type);
   if (value !== null) {
     const textNode = document.createTextNode(value);
